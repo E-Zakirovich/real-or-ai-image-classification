3ece22d764b26d4b45f3628933577212fde6ce4d
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-12 17:02:15 +0500

    dataset paths stored

---
+++ configs.py
@@ -0,0 +1,11 @@
+"""
+configs.py
+~~~~~~~~~~~
+
+I need to store static settings to somewhere, so I created
+configs.py file.
+"""
+
+# dataset paths
+train_and_validation_data_path = "./data/train"
+test_data_path = "./data/test"
