97f9c8ff5c5e2bc0eaa9afb1c5f93caa7cc8795b
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-12 16:41:10 +0500

    I created a file called methods to wrote all methods that used inside of load.py

---
+++ data-loader/methods.py
@@ -0,0 +1,17 @@
+"""
+methods.py
+~~~~~~~~~~~
+
+I created this file to write important methods that used in
+loader.py file. Basically, I could write all methods inside
+of loader.py, but I want my code must be easy to understand.
+"""
+
+# load packages
+from torchvision import datasets
+from torch.utils.data import Subset, DataLoader
+
+
+class Methods:
+    def __init__(self):
+        ...
