b0c0d4ad13edf3b56c5933b85a26f55f45ed8f02
Azamat E. Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-13 16:31:13 +0500

    transformation for train dataset part is finished.

--- data-loader/augmentation.py
+++ data-loader/augmentation.py
@@ -9,6 +9,7 @@ making tensor from loaded images.
 
 # load packages
 from torchvision import transforms
+import configs
 
 
 class Augmentation:
@@ -20,7 +21,35 @@ class Augmentation:
 
     @staticmethod
     def transform_for_train_dataset(path):
-        return ...
+
+        # transformation code for transforms
+        result = transforms.Compose([
+
+            # change the size of the image
+            transforms.Resize(
+                (
+                    configs.image_size, # amount
+                    configs.image_size, # amount
+                )
+            ),
+
+            # flip the image horizontally in order to avoid overfitting
+            transforms.RandomHorizontalFlip(
+                p = configs.horizontal_flip, # amount
+            ),
+
+            # normalization part of the data
+            transforms.Normalize(
+                mean = configs.mean, # mean
+                std = configs.std, # std
+            ),
+
+            # making tensor from processed images
+            transforms.ToTensor(),
+        ])
+
+        # returning the result
+        return result
 
     @staticmethod
     def transform_for_validation_and_test_dataset(path):
@@ -31,5 +60,5 @@ class Augmentation:
         train_dataset = transform_for_train_dataset(self.train_path)
         validation_dataset = transform_for_validation_and_test_dataset(self.validation_path)
         test_dataset = transform_for_validation_and_test_dataset(self.test_path)
-        
+
         return train_dataset, validation_dataset, test_dataset
