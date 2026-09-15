f128d3c324561cc1d8f14faba96723f83b807bb4
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-15 10:00:41 +0500

    this file is core. this file is where the model lives.

---
+++ model/cnn.py
@@ -0,0 +1,14 @@
+"""
+cnn.py
+~~~~~~~
+
+This file is core of the project. To train my model, basically I used 
+convolutional neural network. My network includes five layers, one extra 
+paddings to improve performance of the model.
+"""
+
+# import libraries
+import torch.nn as neural_networks
+
+class CNN(neural_networks.Module):
+    ...
