b9f354b5ca09170e688525fa307b03f0a459acd5
E-Zakirovich <azamateshqoziyev2@gmail.com>
2026-09-15 10:02:50 +0500

    i will train my model(cnn.py) with this file.

---
+++ model/train.py
@@ -0,0 +1,14 @@
+"""
+train.py
+~~~~~~~~~
+
+I am going to use this file to train my convolutional neural
+netoworks. This file is connected to main.py and cnn.py files.
+"""
+
+import numpy
+import torch.optim as optimize
+import torch.nn  as neural_networks
+
+class Train:
+    ...
